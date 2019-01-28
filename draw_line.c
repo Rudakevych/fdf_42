d9e87fe054da05fd1d9cd32141a0b51fbe0e3063
yrudakev <yurij.rudakevych@gmail.com>
2019-01-28 14:08:25 +0200

    trying to find a problem with square map

--- draw_line.c
+++ draw_line.c
@@ -18,75 +18,77 @@
 ** https://en.wikipedia.org/wiki/Bresenham%27s_line_algorithm
 */
 
-void	draw_line_down(t_d_s_points ds_points, t_coordinates *strt, t_map_mlx mlx)
-{
-	int		d;
-	int		d1;
-	int		d2;
-	int		x;
-	int		y;
+//void	draw_line_down(t_d_s_points ds_points, t_coordinates *strt, t_map_mlx mlx)
+//{
+//	int		d;
+//	int		d1;
+//	int		d2;
+//	int		x;
+//	int		y;
+//
+//	x = strt->x + ds_points.sx;
+//	y = strt->y;
+//	d = (ds_points.dy << 1) - ds_points.dx;
+//	d1 = ds_points.dy << 1;
+//	d2 = (ds_points.dy - ds_points.dx) << 1;
+//	mlx_pixel_put(mlx.mlx_ptr, mlx.win_ptr, strt->x + 200, strt->y + 200, strt->color);
+//	while (ds_points.i <= ds_points.dx)
+//	{
+//		if (d > 0)
+//		{
+//			d += d2;
+//			y += ds_points.sy;
+//		}
+//		else
+//			d += d1;
+//		mlx_pixel_put(mlx.mlx_ptr, mlx.win_ptr, x + 200, y + 200, strt->color);
+//		ds_points.i++;
+//		x += ds_points.sx;
+//	}
+//}
+//
+//void	draw_line_up(t_d_s_points ds_points, t_coordinates *strt, t_map_mlx mlx)
+//{
+//	int d;
+//	int d1;
+//	int d2;
+//	int y;
+//	int x;
+//
+//	y = strt->y + ds_points.sy;
+//	x = strt->x;
+//	d = (ds_points.dx << 1) - ds_points.dy;
+//	d1 = ds_points.dx << 1;
+//	d2 = (ds_points.dx - ds_points.dy) << 1;
+//	mlx_pixel_put(mlx.mlx_ptr, mlx.win_ptr, strt->x + 200, strt->y + 200, strt->color);
+//	while (ds_points.i <= ds_points.dy)
+//	{
+//		if (d > 0)
+//		{
+//			d += d2;
+//			x += ds_points.sx;
+//		}
+//		else
+//			d += d1;
+//		mlx_pixel_put(mlx.mlx_ptr, mlx.win_ptr, x + 200, y + 200, strt->color);
+//		ds_points.i++;
+//		y += ds_points.sy;
+//	}
+//}
+//
+//void	draw_line(t_coordinates *start, t_coordinates *end, t_map_mlx mlx)
+//{
+//	t_d_s_points	ds_points;
+//
+//	ds_points.dx = abs(end->x - start->x);
+//	ds_points.dy = abs(end->y - start->y);
+//	ds_points.sx = end->x >= start->x ? 1 : -1;
+//	ds_points.sy = end->y >= start->y ? 1 : -1;
+//	ds_points.i = 1;
+//	if (ds_points.dy <= ds_points.dx)
+//		draw_line_down(ds_points, start, mlx);
+//	else
+//		draw_line_up(ds_points, start, mlx);
+//}
 
-	x = strt->x + ds_points.sx;
-	y = strt->y;
-	d = (ds_points.dy << 1) - ds_points.dx;
-	d1 = ds_points.dy << 1;
-	d2 = (ds_points.dy - ds_points.dx) << 1;
-	mlx_pixel_put(mlx.mlx_ptr, mlx.win_ptr, strt->x + 200, strt->y + 200, strt->color);
-	while (ds_points.i <= ds_points.dx)
-	{
-		if (d > 0)
-		{
-			d += d2;
-			y += ds_points.sy;
-		}
-		else
-			d += d1;
-		mlx_pixel_put(mlx.mlx_ptr, mlx.win_ptr, x + 200, y + 200, strt->color);
-		ds_points.i++;
-		x += ds_points.sx;
-	}
-}
 
-void	draw_line_up(t_d_s_points ds_points, t_coordinates *strt, t_map_mlx mlx)
-{
-	int d;
-	int d1;
-	int d2;
-	int y;
-	int x;
-
-	y = strt->y + ds_points.sy;
-	x = strt->x;
-	d = (ds_points.dx << 1) - ds_points.dy;
-	d1 = ds_points.dx << 1;
-	d2 = (ds_points.dx - ds_points.dy) << 1;
-	mlx_pixel_put(mlx.mlx_ptr, mlx.win_ptr, strt->x + 200, strt->y + 200, strt->color);
-	while (ds_points.i <= ds_points.dy)
-	{
-		if (d > 0)
-		{
-			d += d2;
-			x += ds_points.sx;
-		}
-		else
-			d += d1;
-		mlx_pixel_put(mlx.mlx_ptr, mlx.win_ptr, x + 200, y + 200, strt->color);
-		ds_points.i++;
-		y += ds_points.sy;
-	}
-}
-
-void	draw_line(t_coordinates *start, t_coordinates *end, t_map_mlx mlx)
-{
-	t_d_s_points	ds_points;
-
-	ds_points.dx = abs(end->x - start->x);
-	ds_points.dy = abs(end->y - start->y);
-	ds_points.sx = end->x >= start->x ? 1 : -1;
-	ds_points.sy = end->y >= start->y ? 1 : -1;
-	ds_points.i = 1;
-	if (ds_points.dy <= ds_points.dx)
-		draw_line_down(ds_points, start, mlx);
-	else
-		draw_line_up(ds_points, start, mlx);
-}
--- draw_net.c
+++ draw_net.c
@@ -52,12 +52,10 @@ void	draw_net(t_map_mlx map)
 		j = 0;
 		while (j < map.columns_nbr - 1)
 		{
-//			printf("(%d,%d)     ", i, j);
 			if (i == map.columns_nbr - 1 && j == map.columns_nbr - 1)
 				break ;
 			if (j != map.lines_nbr - 1)
 			{
-//				printf("adsfg\n");
 				start_h.x = map.arr_of_coordinates[i][j].x * 20;
 				start_h.y = map.arr_of_coordinates[i][j].y * 20;
 				start_h.color = 0xFFFFFF; // add correct usage
@@ -70,7 +68,6 @@ void	draw_net(t_map_mlx map)
 			}
 			if (j != map.columns_nbr - 1)
 			{
-//				printf("1111111\n");
 				start_h.x = map.arr_of_coordinates[i][j].x * 20;
 				start_h.y = map.arr_of_coordinates[i][j].y * 20;
 				start_h.color = 0xFFFFFF; // add correct usage
