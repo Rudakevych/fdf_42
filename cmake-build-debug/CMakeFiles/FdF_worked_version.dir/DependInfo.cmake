b747dd529c9a713d328a38a79fd50e8c57ab22bb
yrudakev <yurij.rudakevych@gmail.com>
2019-03-23 14:15:57 +0200

    added version 3

---
+++ cmake-build-debug/CMakeFiles/FdF_worked_version.dir/DependInfo.cmake
@@ -0,0 +1,49 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/yrudakev/CLionProjects/FdF_worked_version/calculations_in_matrix.c" "/Users/yrudakev/CLionProjects/FdF_worked_version/cmake-build-debug/CMakeFiles/FdF_worked_version.dir/calculations_in_matrix.c.o"
+  "/Users/yrudakev/CLionProjects/FdF_worked_version/draw_line.c" "/Users/yrudakev/CLionProjects/FdF_worked_version/cmake-build-debug/CMakeFiles/FdF_worked_version.dir/draw_line.c.o"
+  "/Users/yrudakev/CLionProjects/FdF_worked_version/draw_map.c" "/Users/yrudakev/CLionProjects/FdF_worked_version/cmake-build-debug/CMakeFiles/FdF_worked_version.dir/draw_map.c.o"
+  "/Users/yrudakev/CLionProjects/FdF_worked_version/draw_net.c" "/Users/yrudakev/CLionProjects/FdF_worked_version/cmake-build-debug/CMakeFiles/FdF_worked_version.dir/draw_net.c.o"
+  "/Users/yrudakev/CLionProjects/FdF_worked_version/draw_window.c" "/Users/yrudakev/CLionProjects/FdF_worked_version/cmake-build-debug/CMakeFiles/FdF_worked_version.dir/draw_window.c.o"
+  "/Users/yrudakev/CLionProjects/FdF_worked_version/errors_all.c" "/Users/yrudakev/CLionProjects/FdF_worked_version/cmake-build-debug/CMakeFiles/FdF_worked_version.dir/errors_all.c.o"
+  "/Users/yrudakev/CLionProjects/FdF_worked_version/free_for_all.c" "/Users/yrudakev/CLionProjects/FdF_worked_version/cmake-build-debug/CMakeFiles/FdF_worked_version.dir/free_for_all.c.o"
+  "/Users/yrudakev/CLionProjects/FdF_worked_version/get_next_line.c" "/Users/yrudakev/CLionProjects/FdF_worked_version/cmake-build-debug/CMakeFiles/FdF_worked_version.dir/get_next_line.c.o"
+  "/Users/yrudakev/CLionProjects/FdF_worked_version/hook.c" "/Users/yrudakev/CLionProjects/FdF_worked_version/cmake-build-debug/CMakeFiles/FdF_worked_version.dir/hook.c.o"
+  "/Users/yrudakev/CLionProjects/FdF_worked_version/libft/ft_atoi.c" "/Users/yrudakev/CLionProjects/FdF_worked_version/cmake-build-debug/CMakeFiles/FdF_worked_version.dir/libft/ft_atoi.c.o"
+  "/Users/yrudakev/CLionProjects/FdF_worked_version/libft/ft_bzero.c" "/Users/yrudakev/CLionProjects/FdF_worked_version/cmake-build-debug/CMakeFiles/FdF_worked_version.dir/libft/ft_bzero.c.o"
+  "/Users/yrudakev/CLionProjects/FdF_worked_version/libft/ft_lstadd.c" "/Users/yrudakev/CLionProjects/FdF_worked_version/cmake-build-debug/CMakeFiles/FdF_worked_version.dir/libft/ft_lstadd.c.o"
+  "/Users/yrudakev/CLionProjects/FdF_worked_version/libft/ft_lstnew.c" "/Users/yrudakev/CLionProjects/FdF_worked_version/cmake-build-debug/CMakeFiles/FdF_worked_version.dir/libft/ft_lstnew.c.o"
+  "/Users/yrudakev/CLionProjects/FdF_worked_version/libft/ft_memalloc.c" "/Users/yrudakev/CLionProjects/FdF_worked_version/cmake-build-debug/CMakeFiles/FdF_worked_version.dir/libft/ft_memalloc.c.o"
+  "/Users/yrudakev/CLionProjects/FdF_worked_version/libft/ft_memcpy.c" "/Users/yrudakev/CLionProjects/FdF_worked_version/cmake-build-debug/CMakeFiles/FdF_worked_version.dir/libft/ft_memcpy.c.o"
+  "/Users/yrudakev/CLionProjects/FdF_worked_version/libft/ft_memset.c" "/Users/yrudakev/CLionProjects/FdF_worked_version/cmake-build-debug/CMakeFiles/FdF_worked_version.dir/libft/ft_memset.c.o"
+  "/Users/yrudakev/CLionProjects/FdF_worked_version/libft/ft_putstr.c" "/Users/yrudakev/CLionProjects/FdF_worked_version/cmake-build-debug/CMakeFiles/FdF_worked_version.dir/libft/ft_putstr.c.o"
+  "/Users/yrudakev/CLionProjects/FdF_worked_version/libft/ft_strchr.c" "/Users/yrudakev/CLionProjects/FdF_worked_version/cmake-build-debug/CMakeFiles/FdF_worked_version.dir/libft/ft_strchr.c.o"
+  "/Users/yrudakev/CLionProjects/FdF_worked_version/libft/ft_strcmp.c" "/Users/yrudakev/CLionProjects/FdF_worked_version/cmake-build-debug/CMakeFiles/FdF_worked_version.dir/libft/ft_strcmp.c.o"
+  "/Users/yrudakev/CLionProjects/FdF_worked_version/libft/ft_strcpy.c" "/Users/yrudakev/CLionProjects/FdF_worked_version/cmake-build-debug/CMakeFiles/FdF_worked_version.dir/libft/ft_strcpy.c.o"
+  "/Users/yrudakev/CLionProjects/FdF_worked_version/libft/ft_strdel.c" "/Users/yrudakev/CLionProjects/FdF_worked_version/cmake-build-debug/CMakeFiles/FdF_worked_version.dir/libft/ft_strdel.c.o"
+  "/Users/yrudakev/CLionProjects/FdF_worked_version/libft/ft_strdup.c" "/Users/yrudakev/CLionProjects/FdF_worked_version/cmake-build-debug/CMakeFiles/FdF_worked_version.dir/libft/ft_strdup.c.o"
+  "/Users/yrudakev/CLionProjects/FdF_worked_version/libft/ft_strjoin.c" "/Users/yrudakev/CLionProjects/FdF_worked_version/cmake-build-debug/CMakeFiles/FdF_worked_version.dir/libft/ft_strjoin.c.o"
+  "/Users/yrudakev/CLionProjects/FdF_worked_version/libft/ft_strlen.c" "/Users/yrudakev/CLionProjects/FdF_worked_version/cmake-build-debug/CMakeFiles/FdF_worked_version.dir/libft/ft_strlen.c.o"
+  "/Users/yrudakev/CLionProjects/FdF_worked_version/libft/ft_strnew.c" "/Users/yrudakev/CLionProjects/FdF_worked_version/cmake-build-debug/CMakeFiles/FdF_worked_version.dir/libft/ft_strnew.c.o"
+  "/Users/yrudakev/CLionProjects/FdF_worked_version/libft/ft_strsplit.c" "/Users/yrudakev/CLionProjects/FdF_worked_version/cmake-build-debug/CMakeFiles/FdF_worked_version.dir/libft/ft_strsplit.c.o"
+  "/Users/yrudakev/CLionProjects/FdF_worked_version/libft/ft_strsub.c" "/Users/yrudakev/CLionProjects/FdF_worked_version/cmake-build-debug/CMakeFiles/FdF_worked_version.dir/libft/ft_strsub.c.o"
+  "/Users/yrudakev/CLionProjects/FdF_worked_version/main.c" "/Users/yrudakev/CLionProjects/FdF_worked_version/cmake-build-debug/CMakeFiles/FdF_worked_version.dir/main.c.o"
+  "/Users/yrudakev/CLionProjects/FdF_worked_version/one_more_file.c" "/Users/yrudakev/CLionProjects/FdF_worked_version/cmake-build-debug/CMakeFiles/FdF_worked_version.dir/one_more_file.c.o"
+  "/Users/yrudakev/CLionProjects/FdF_worked_version/read_map.c" "/Users/yrudakev/CLionProjects/FdF_worked_version/cmake-build-debug/CMakeFiles/FdF_worked_version.dir/read_map.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/FdF_worked_version.dir/cmake_clean.cmake
@@ -0,0 +1,39 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FdF_worked_version.dir/main.c.o"
+  "CMakeFiles/FdF_worked_version.dir/draw_window.c.o"
+  "CMakeFiles/FdF_worked_version.dir/hook.c.o"
+  "CMakeFiles/FdF_worked_version.dir/draw_map.c.o"
+  "CMakeFiles/FdF_worked_version.dir/calculations_in_matrix.c.o"
+  "CMakeFiles/FdF_worked_version.dir/errors_all.c.o"
+  "CMakeFiles/FdF_worked_version.dir/one_more_file.c.o"
+  "CMakeFiles/FdF_worked_version.dir/read_map.c.o"
+  "CMakeFiles/FdF_worked_version.dir/libft/ft_strcmp.c.o"
+  "CMakeFiles/FdF_worked_version.dir/libft/ft_lstadd.c.o"
+  "CMakeFiles/FdF_worked_version.dir/get_next_line.c.o"
+  "CMakeFiles/FdF_worked_version.dir/libft/ft_strcpy.c.o"
+  "CMakeFiles/FdF_worked_version.dir/libft/ft_strdel.c.o"
+  "CMakeFiles/FdF_worked_version.dir/libft/ft_strnew.c.o"
+  "CMakeFiles/FdF_worked_version.dir/libft/ft_memcpy.c.o"
+  "CMakeFiles/FdF_worked_version.dir/libft/ft_lstnew.c.o"
+  "CMakeFiles/FdF_worked_version.dir/libft/ft_strjoin.c.o"
+  "CMakeFiles/FdF_worked_version.dir/libft/ft_memset.c.o"
+  "CMakeFiles/FdF_worked_version.dir/libft/ft_memalloc.c.o"
+  "CMakeFiles/FdF_worked_version.dir/libft/ft_strsub.c.o"
+  "CMakeFiles/FdF_worked_version.dir/libft/ft_putstr.c.o"
+  "CMakeFiles/FdF_worked_version.dir/libft/ft_bzero.c.o"
+  "CMakeFiles/FdF_worked_version.dir/libft/ft_strchr.c.o"
+  "CMakeFiles/FdF_worked_version.dir/libft/ft_strdup.c.o"
+  "CMakeFiles/FdF_worked_version.dir/libft/ft_strlen.c.o"
+  "CMakeFiles/FdF_worked_version.dir/libft/ft_atoi.c.o"
+  "CMakeFiles/FdF_worked_version.dir/libft/ft_strsplit.c.o"
+  "CMakeFiles/FdF_worked_version.dir/free_for_all.c.o"
+  "CMakeFiles/FdF_worked_version.dir/draw_net.c.o"
+  "CMakeFiles/FdF_worked_version.dir/draw_line.c.o"
+  "FdF_worked_version.pdb"
+  "FdF_worked_version"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/FdF_worked_version.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
